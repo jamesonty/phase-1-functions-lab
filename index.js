104cfe65096d2ad9c6ec3e89824221493ba6d3a1
JamesMwangi <jamohgamer@gmail.com>
2024-03-27 20:27:02 +0300

    functions-lab completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    return Math.abs(distance - 42)
+}
+
+function distanceFromHqInFeet(distance){
+    return distanceFromHqInBlocks(distance) * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((destination - start)) * 264
+}
+
+function calculatesFarePrice(start , destination){
+
+    let feet = distanceTravelledInFeet(start,destination);
+    if(feet <= 400) return 0
+    else if (feet >400 && feet<=2000) return (0.02 * (feet-400))
+    else if (feet > 2000 && feet <= 2500) return 25
+    else return 'cannot travel that far'
+}
